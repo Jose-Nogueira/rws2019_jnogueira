94fe9ed243db22f71d6174703f759ebcddfaabdb
José Nogueira <jnogueira1994@hotmail.com>
2019-02-21 16:59:24 +0000

    init_4

--- player_jnogueira/src/player_jnogueira.cpp
+++ player_jnogueira/src/player_jnogueira.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <rws2019_msgs/MakeAPlay.h>
 #include <iostream>
 #include <vector>
 
@@ -10,140 +11,150 @@ namespace jnogueira_ns
 class Team
 {
 public:
-  Team(string Team_name_)
-  {
-	Team_name = Team_name_;
-	ros::NodeHandle n;
-	n.getParam("/team_" + Team_name, players);
-  }
-  void addPlayer(string name)
-  {
-	players.push_back(name);
-  }
-  void printInfo()
-  {
-	ROS_INFO_STREAM("Team " << Team_name);
-	for (size_t i = 0; i < players.size(); i++)
+	Team(string Team_name_)
 	{
-	  ROS_INFO_STREAM(players[i]);
+		Team_name = Team_name_;
+		ros::NodeHandle n;
+		n.getParam("/team_" + Team_name, players);
 	}
-  }
-  bool playerBelongsToTeam(string name)
-  {
-	for (size_t i = 0; i < players.size(); i++)
+	void addPlayer(string name)
 	{
-	  if (players[i] == name)
-		return true;
+		players.push_back(name);
 	}
-	return false;
-  }
-  string Team_name;
+	void printInfo()
+	{
+		ROS_INFO_STREAM("Team " << Team_name);
+		for (size_t i = 0; i < players.size(); i++)
+		{
+			ROS_INFO_STREAM(players[i]);
+		}
+	}
+	bool playerBelongsToTeam(string name)
+	{
+		for (size_t i = 0; i < players.size(); i++)
+		{
+			if (players[i] == name)
+				return true;
+		}
+		return false;
+	}
+	string Team_name;
 
 private:
-  vector<string> players;
+	vector<string> players;
 };
 class Player
 {
 public:
-  Player(string player_name_)
-  {
-	player_name = player_name_;
-  }
+	Player(string player_name_)
+	{
+		player_name = player_name_;
+	}
 
-  void setTeamName(string name)
-  {
-	if (name == "red" || name == "blue" || name == "green")
+	void setTeamName(string name)
 	{
-	  team = name;
+		if (name == "red" || name == "blue" || name == "green")
+		{
+			team = name;
+		}
+		else
+		{
+			ROS_ERROR_STREAM("Cannot set team name ");
+		}
 	}
-	else
+	void setTeamName(int index)
 	{
-	  ROS_ERROR_STREAM("Cannot set team name ");
+		if (index == 0)
+			setTeamName("red");
+		else if (index == 1)
+			setTeamName("green");
+		else if (index == 2)
+			setTeamName("blue");
+		else
+			setTeamName("");
 	}
-  }
-  void setTeamName(int index)
-  {
-	if (index == 0)
-	  setTeamName("red");
-	else if (index == 1)
-	  setTeamName("green");
-	else if (index == 2)
-	  setTeamName("blue");
-	else
-	  setTeamName("");
-  }
-  string getTeam()
-  {
-	return team;
-  }
-  string player_name;
+	string getTeam()
+	{
+		return team;
+	}
+	string player_name;
 
 private:
-  int a;
-  string team;
+	int a;
+	string team;
 };
 class MyPlayer : public Player
 {
 public:
-  MyPlayer(string player_name_) : Player(player_name_)
-  {
-	team_red = (boost::shared_ptr<Team>)new Team("red");
-	team_blue = (boost::shared_ptr<Team>)new Team("blue");
-	team_green = (boost::shared_ptr<Team>)new Team("green");
-	if (team_red->playerBelongsToTeam(player_name_))
+	MyPlayer(string player_name_) : Player(player_name_)
 	{
-	  team_hunter = team_blue;
-	  team_my = team_red;
-	  team_preys = team_green;
+		team_red = (boost::shared_ptr<Team>)new Team("red");
+		team_blue = (boost::shared_ptr<Team>)new Team("blue");
+		team_green = (boost::shared_ptr<Team>)new Team("green");
+		if (team_red->playerBelongsToTeam(player_name_))
+		{
+			team_hunter = team_blue;
+			team_my = team_red;
+			team_preys = team_green;
+		}
+		else if (team_green->playerBelongsToTeam(player_name_))
+		{
+			team_hunter = team_red;
+			team_my = team_green;
+			team_preys = team_blue;
+		}
+		else if (team_blue->playerBelongsToTeam(player_name_))
+		{
+			team_hunter = team_green;
+			team_my = team_blue;
+			team_preys = team_red;
+		}
+		else
+		{
+			ROS_ERROR_STREAM("Player not have team");
+		}
+		setTeamName(team_my->Team_name);
 	}
-	else if (team_green->playerBelongsToTeam(player_name_))
+	void makeAPlayCallback(rws2019_msgs::MakeAPlayConstPtr make_a_play)
 	{
-	  team_hunter = team_red;
-	  team_my = team_green;
-	  team_preys = team_blue;
+		ROS_INFO_STREAM("Cat_vel: " << make_a_play->cat);
+		ROS_INFO_STREAM("Dog_vel: " << make_a_play->dog);
+		ROS_INFO_STREAM("Cheetah_vel: " << make_a_play->cheetah);
+		ROS_INFO_STREAM("Turtle_vel: " << make_a_play->turtle);
 	}
-	else if (team_blue->playerBelongsToTeam(player_name_))
-	{
-	  team_hunter = team_green;
-	  team_my = team_blue;
-	  team_preys = team_red;
-	}
-	else
-	{
-	  ROS_ERROR_STREAM("Player not have team");
-	}
-	setTeamName(team_my->Team_name);
-  }
 
-  boost::shared_ptr<Team> team_red;
-  boost::shared_ptr<Team> team_blue;
-  boost::shared_ptr<Team> team_green;
-  boost::shared_ptr<Team> team_hunter;
-  boost::shared_ptr<Team> team_my;
-  boost::shared_ptr<Team> team_preys;
+	boost::shared_ptr<Team> team_red;
+	boost::shared_ptr<Team> team_blue;
+	boost::shared_ptr<Team> team_green;
+	boost::shared_ptr<Team> team_hunter;
+	boost::shared_ptr<Team> team_my;
+	boost::shared_ptr<Team> team_preys;
 };
 }
 #pragma endregion
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "player_jnogueira");
-  ros::NodeHandle nn;
+	ros::init(argc, argv, "player_jnogueira");
+	ros::NodeHandle nn;
 
-  jnogueira_ns::MyPlayer player("jnogueira");
+	jnogueira_ns::MyPlayer player("jnogueira");
 
-  player.team_red->printInfo();
-  cout << endl;
-  player.team_blue->printInfo();
-  cout << endl;
-  player.team_green->printInfo();
+	player.team_red->printInfo();
+	cout << endl;
+	player.team_blue->printInfo();
+	cout << endl;
+	player.team_green->printInfo();
 
-  ros::Rate l(1);
-  while (ros::ok())
-  {
-	ROS_INFO_STREAM("\tName: " + player.player_name + "\tTeam: " + player.getTeam());
-	l.sleep();
-  }
+	ros::Subscriber sub = nn.subscribe("/make_a_play", 100, &jnogueira_ns::MyPlayer::makeAPlayCallback, &player);
 
-  return 1;
+	ros::Rate l(10);
+	while (ros::ok())
+	{
+		// ROS_INFO_STREAM("\tName: " + player.player_name + "\tTeam: " + player.getTeam());
+		l.sleep();
+		ros::spinOnce();
+	}
+
+	return 1;
 }
